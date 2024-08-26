211a630ce10d71ba8e8085776741991316fc3a13
mdakbarhossain1 <akbarshovo18@gmail.com>
2024-08-26 12:30:55 +0600

    netlify

--- model/User.js
+++ model/User.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
+
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     return next();
